9fc3b6cc2cec04efc0bbef8300ea6a645c899d75
Kieran <Kieranherr@gmail.com>
2020-08-24 15:36:14 -0500

    Init Commit

---
+++ CustomListUnitTest/AddTesting.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    public class AddTesting
+    {
+        [TestMethod]
+        public void Add_AddTwoToList_Count2()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 4;
+            int value2 = 5;
+            int expected = 2;
+            int actual;
+
+            customList.Add(value1);
+            customList.Add(value2);
+            actual = customList.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddTwoToList_Capacity4()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 4;
+            int value2 = 5;
+            int expected = 4;
+            int actual;
+
+            customList.Add(value1);
+            customList.Add(value2);
+            actual = customList.Capacity();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_TempArray_Index()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 4;
+            int value2 = 5;
+            int value3 = 7;
+            int value4 = 9;
+            int value5 = 12;
+            int expected = 7;
+            int actual;
+
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+            customList.Add(value4);
+            customList.Add(value5);
+            actual = customList[2];
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddToList_Count10()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 4;
+            int value2 = 5;
+            int value3 = 7;
+            int value4 = 9;
+            int value5 = 12;
+            int value6 = 4;
+            int value7 = 5;
+            int value8 = 7;
+            int value9 = 9;
+            int value10 = 12;
+            int expected = 10;
+            int actual;
+
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+            customList.Add(value4);
+            customList.Add(value5);
+            customList.Add(value6);
+            customList.Add(value7);
+            customList.Add(value8);
+            customList.Add(value9);
+            customList.Add(value10);
+            actual = customList.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CustomListUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListUnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5d23789-da8c-4bd8-8d40-99ba97757de3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
