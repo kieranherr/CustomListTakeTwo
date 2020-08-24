00238d2bb2f738977ea9566a8ee828bbbadb72f3
Kieran <Kieranherr@gmail.com>
2020-08-24 16:47:26 -0500

    Added index remove method

--- CustomListUnitTest/RemoveTesting.cs
+++ CustomListUnitTest/RemoveTesting.cs
@@ -29,6 +29,7 @@ namespace CustomListUnitTest
         [TestMethod]
         public void Remove_RemoveTwoFromList_Capacity4()
         {
+            CustomList<int> customList = new CustomList<int>();
             int value1 = 6;
             int expected = 4;
             int actual;
@@ -47,6 +48,7 @@ namespace CustomListUnitTest
         [TestMethod]
         public void Remove_RemoveTwoFromList_Capacity8()
         {
+            CustomList<int> customList = new CustomList<int>();
             int value1 = 6;
             int expected = 8;
             int actual;
@@ -79,6 +81,26 @@ namespace CustomListUnitTest
 
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void Remove_RemoveFromList_Index0()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 6;
+            int value2 = 5;
+            int value3 = 7;
+            int expected = 0;
+            int actual;
 
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+            customList.Add(value4);
+
+            customList.Remove(value1);
+
+            actual = customList.IndexOf(value2);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
